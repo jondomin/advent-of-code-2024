31d068b5f6ac11e09a96294b2343525c6db9f89a
Jonathan Dominguez <jonathan.dominguez@cellares.com>
2024-12-01 15:48:30 -0600

    part 1

--- 01/index.ts
+++ 01/index.ts
@@ -1,3 +1,12 @@
+import { zip } from "@std/collections";
 export default function solution(a: number[], b: number[]) {
-	return "hello world";
+  const sortedA = a.sort();
+  const sortedB = b.sort();
+
+  const result = zip(sortedA, sortedB).map((pair) => {
+    return Math.abs(pair[0] - pair[1]);
+  });
+
+  return result;
 }
+export function readFile() {}
--- 01/index_test.ts
+++ 01/index_test.ts
@@ -1,9 +1,47 @@
-import { assertEquals } from "@std/assert";
+import { assertEquals, assertGreater } from "@std/assert";
 import solution from "./index.ts";
 
-Deno.test("Day 1", async (t) => {
-	await t.step("solution", () => {
-		const result = solution();
-		assertEquals(result, "hello world");
-	});
-})
+Deno.test("Day 1 - Test", async (t) => {
+  const a = [3, 4, 2, 1, 3, 3];
+  const b = [4, 3, 5, 3, 9, 3];
+  const result = solution(a, b);
+  assertEquals(2, result[0]);
+  assertEquals(1, result[1]);
+  assertEquals(0, result[2]);
+  assertEquals(1, result[3]);
+  assertEquals(2, result[4]);
+  assertEquals(5, result[5]);
+});
+Deno.test("Day 1 - Part 1", async (t) => {
+  const content = await Deno.readTextFile("01/input.txt");
+  const a: number[] = [];
+  const b: number[] = [];
+  content.split("\n").forEach((line) => {
+    const v = line.trim().split(/\s+/).map(Number);
+    a.push(v[0]);
+    b.push(v[1]);
+  });
+  const result = solution(a, b);
+  const calc = result.reduce((prev, current) => prev + current, 0);
+  // add all the results together
+
+  assertGreater(calc, 0);
+  console.log(calc);
+});
+
+Deno.test("Day 1 - Part 2", async (t) => {
+  const content = await Deno.readTextFile("01/input.txt");
+  const a: number[] = [];
+  const b: number[] = [];
+  content.split("\n").forEach((line) => {
+    const v = line.trim().split(/\s+/).map(Number);
+    a.push(v[0]);
+    b.push(v[1]);
+  });
+  const result = solution(a, b);
+  const calc = result.reduce((prev, current) => prev + current, 0);
+  // add all the results together
+
+  assertGreater(calc, 0);
+  console.log(calc);
+});
