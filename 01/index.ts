60ede3737fea191e4a99cd34c154336bf4245b51
Jonathan Dominguez <jonathan.dominguez@cellares.com>
2024-12-01 16:28:52 -0600

    day 1

--- 01/index.ts
+++ 01/index.ts
@@ -9,4 +9,35 @@ export default function solution(a: number[], b: number[]) {
 
   return result;
 }
-export function readFile() {}
+
+export function solution2(a: number[], b: number[]): number {
+  const occ = occurrences(b);
+  return a.reduce((prev, current) => {
+    if (occ[current]) {
+      return prev + current * occ[current];
+    }
+    return prev;
+  }, 0);
+}
+
+function occurrences(b: number[]) {
+  const hash: Record<string, number> = {};
+  b.forEach((num) => {
+    hash[num] = (hash[num] || 0) + 1;
+  });
+  return hash;
+}
+
+export async function readFile(
+  filePath: string
+): Promise<[number[], number[]]> {
+  const content = await Deno.readTextFile(filePath);
+  const a: number[] = [];
+  const b: number[] = [];
+  content.split("\n").forEach((line) => {
+    const v = line.trim().split(/\s+/).map(Number);
+    a.push(v[0]);
+    b.push(v[1]);
+  });
+  return [a, b];
+}
--- 01/index_test.ts
+++ 01/index_test.ts
@@ -1,9 +1,8 @@
 import { assertEquals, assertGreater } from "@std/assert";
-import solution from "./index.ts";
+import solution, { readFile, solution2 } from "./index.ts";
 
-Deno.test("Day 1 - Test", async (t) => {
-  const a = [3, 4, 2, 1, 3, 3];
-  const b = [4, 3, 5, 3, 9, 3];
+Deno.test("Day 1 - Test", async () => {
+  const [a, b] = await readFile("01/test.txt");
   const result = solution(a, b);
   assertEquals(2, result[0]);
   assertEquals(1, result[1]);
@@ -12,36 +11,25 @@ Deno.test("Day 1 - Test", async (t) => {
   assertEquals(2, result[4]);
   assertEquals(5, result[5]);
 });
-Deno.test("Day 1 - Part 1", async (t) => {
-  const content = await Deno.readTextFile("01/input.txt");
-  const a: number[] = [];
-  const b: number[] = [];
-  content.split("\n").forEach((line) => {
-    const v = line.trim().split(/\s+/).map(Number);
-    a.push(v[0]);
-    b.push(v[1]);
-  });
+Deno.test("Day 1 - Part 1", async () => {
+  const [a, b] = await readFile("01/input.txt");
   const result = solution(a, b);
   const calc = result.reduce((prev, current) => prev + current, 0);
-  // add all the results together
 
   assertGreater(calc, 0);
   console.log(calc);
 });
 
-Deno.test("Day 1 - Part 2", async (t) => {
-  const content = await Deno.readTextFile("01/input.txt");
-  const a: number[] = [];
-  const b: number[] = [];
-  content.split("\n").forEach((line) => {
-    const v = line.trim().split(/\s+/).map(Number);
-    a.push(v[0]);
-    b.push(v[1]);
-  });
-  const result = solution(a, b);
-  const calc = result.reduce((prev, current) => prev + current, 0);
-  // add all the results together
+Deno.test("Day 1 - Test 2", async () => {
+  const [a, b] = await readFile("01/test.txt");
+  const calc = solution2(a, b);
+  assertGreater(calc, 0);
+  console.log(calc);
+});
 
+Deno.test("Day 1 - Part 2", async () => {
+  const [a, b] = await readFile("01/input.txt");
+  const calc = solution2(a, b);
   assertGreater(calc, 0);
   console.log(calc);
 });
