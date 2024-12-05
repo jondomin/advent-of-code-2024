7ee2d9bd04949881de5c0039a3921242fd35cc32
Jonathan Dominguez <jonathan.dominguez@cellares.com>
2024-12-04 23:32:37 -0600

    final day 2

--- 02/index.ts
+++ 02/index.ts
@@ -3,7 +3,7 @@ export function solution(list: number[][]) {
     // if the array has only one element, it is a valid path
     if (arr.length < 2) return acc + 1;
     // if the array has only two elements, check if the distance is valid
-    if (arr.length < 3) isValidDist(arr[0], arr[1]) ? acc + 1 : acc;
+    if (arr.length < 3) isValidDistance(arr[0], arr[1]) ? acc + 1 : acc;
     // check if array has a valid path
     for (let i = 1; i < arr.length - 1; i++) {
       const curr = arr[i];
@@ -14,8 +14,8 @@ export function solution(list: number[][]) {
       if (
         (leftDiff > 0 && rightDiff < 0) ||
         (leftDiff < 0 && rightDiff > 0) ||
-        !isValidDiff(leftDiff) ||
-        !isValidDiff(rightDiff)
+        !isValidDifference(leftDiff) ||
+        !isValidDifference(rightDiff)
       )
         return acc;
     }
@@ -24,44 +24,30 @@ export function solution(list: number[][]) {
 }
 export function solution2(list: number[][]) {
   return list.reduce((acc, arr) => {
-    // if the array has only one element, it is a valid path
-    if (arr.length < 2) return acc + 1;
-    // if the array has only two elements, check if the distance is valid
-    if (arr.length < 3) isValidDist(arr[0], arr[1]) ? acc + 1 : acc;
-    // check if array has a valid path
     let errorCount = 0;
-    let i = 1;
-    while (i < arr.length - 1) {
-      const curr = arr[i];
-      const prev = arr[i - 1];
-      const next = arr[i + 1];
-      const leftDiff = prev - curr;
-      const rightDiff = curr - next;
-      if (isInvalidIncDec(leftDiff, rightDiff) || !isValidDiff(leftDiff) || !isValidDiff(rightDiff)) {
-        if (errorCount > 0) return acc;
-        errorCount++;
-        if (!isValidDiff(rightDiff) && i < arr.length - 2 && isValidDist(curr, arr[i + 2])) {
-          arr.splice(i + 1, 1);
-        } else if (!isValidDiff(leftDiff)) {
-          arr.splice(i - 1, 1);
-        } else {
-          arr.splice(i, 1);
-        }
-        continue;
-      }
-      i++;
+    const orgArr = [...arr];
+    while (true) {
+      const isValid = arr.every((_, i, a) => isValidIncDec(i, a) && isValidDistance(i, a));
+      if (isValid) return acc + 1;
+      arr = orgArr.filter((_, i) => i !== errorCount);
+      errorCount++;
+      if (errorCount > orgArr.length) return acc;
     }
-    return acc + 1;
   }, 0);
 }
-function isInvalidIncDec(leftDiff: number, rightDiff: number) {
-  return (leftDiff > 0 && rightDiff < 0) || (leftDiff < 0 && rightDiff > 0);
+function isValidIncDec(i: number, arr: number[]) {
+  if (i === 0 || i === arr.length - 1) return true;
+  const leftDiff = arr[i - 1] - arr[i];
+  const rightDiff = arr[i] - arr[i + 1];
+  return (leftDiff > 0 && rightDiff > 0) || (leftDiff < 0 && rightDiff < 0);
 }
-function isValidDist(a: number, b: number) {
-  const diff = Math.abs(a - b);
-  return diff >= 1 && diff <= 3;
+function isValidDistance(i: number, arr: number[]) {
+  if (i === 0 || i === arr.length - 1) return true;
+  const leftDiff = arr[i - 1] - arr[i];
+  const rightDiff = arr[i] - arr[i + 1];
+  return Math.abs(leftDiff) >= 1 && Math.abs(leftDiff) <= 3 && Math.abs(rightDiff) >= 1 && Math.abs(rightDiff) <= 3;
 }
-function isValidDiff(a: number) {
+function isValidDifference(a: number) {
   const diff = Math.abs(a);
   return diff >= 1 && diff <= 3;
 }
--- 02/index_test.ts
+++ 02/index_test.ts
@@ -2,27 +2,11 @@ import { assertEquals } from "@std/assert/equals";
 import { solution, solution2 } from "./index.ts";
 import { readFile } from "./utils.ts";
 
-Deno.test("Day 2 - Test 1", async () => {
+Deno.test("Day 2 - Part 1 - Test", async () => {
   // Arrange
   const data = await readFile("02/test.txt");
   // Act
   const result = solution(data);
-  // Assert
-  assertEquals(result, 2);
-});
-Deno.test("Day 2 - Test 2", async () => {
-  // Arrange
-  const data = await readFile("02/test.txt");
-  // Act
-  const result = solution2(data);
-  // Assert
-  assertEquals(result, 4);
-});
-Deno.test("Day 2 - Test 3", async () => {
-  // Arrange
-  const data = await readFile("02/test2.txt");
-  // Act
-  const result = solution2(data);
   // Assert
   assertEquals(result, 2);
 });
@@ -34,6 +18,22 @@ Deno.test("Day 2 - Solution 1", async () => {
   // Assert
   console.log(result);
   assertEquals(result > 0, true);
+});
+Deno.test("Day 2 - Part 2 - Test 1", async () => {
+  // Arrange
+  const data = await readFile("02/test.txt");
+  // Act
+  const result = solution2(data);
+  // Assert
+  assertEquals(result, 4);
+});
+Deno.test("Day 2 - Part 2 - Test 2", async () => {
+  // Arrange
+  const data = await readFile("02/test2.txt");
+  // Act
+  const result = solution2(data);
+  // Assert
+  assertEquals(result, 2);
 });
 Deno.test("Day 2 - Solution 2", async () => {
   // Arrange
